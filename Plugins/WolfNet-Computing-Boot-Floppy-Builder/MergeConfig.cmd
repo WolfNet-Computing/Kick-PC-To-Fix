bb22b60c6084280adf209463506c984ba7c79264
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2023-07-20 03:30:39 +0100

    Update MergeConfig.cmd

--- Plugins/WolfNet-Computing-Boot-Floppy-Builder/MergeConfig.cmd
+++ Plugins/WolfNet-Computing-Boot-Floppy-Builder/MergeConfig.cmd
@@ -1,9 +1,17 @@
 @echo off
-if not exist plugin.cfg (
+if not exist ./bfd.cfg (
+	if exist ./bfd.sam (
+		xcopy ./bfd.sam ./bfd.cfg /Q
+	) else (
+		echo ERROR: No default configuration file to merge into!
+	)
+)
+
+if not exist ./plugin.cfg (
 	echo ERROR: No plugin configuration file to merge with main configuration!
 	goto :eof
 )
 
-for /F "tokens=*" %%i in (plugin.cfg) do (
-	@echo %%i>>bfd.cfg
+for /F "tokens=*" %%i in (./plugin.cfg) do (
+	@echo %%i>>./bfd.cfg
 )
