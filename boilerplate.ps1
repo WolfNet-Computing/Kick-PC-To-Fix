55203b04f7e40aa126171d412602e7c61961da18
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2023-08-01 09:58:11 +0100

    Updated main shell script to Powershell...

---
+++ boilerplate.ps1
@@ -0,0 +1,17 @@
+If (-not (Test-Path -Path VERSION -PathType Leaf)) {
+    Write-Error "VERSION file doesn't exist!"
+	Write-Error "Unknown version error."
+	Exit
+}
+Else {
+    Set-Variable -Name _version -Value (Get-Content -Path VERSION)
+}
+
+$Host.UI.RawUI.BackgroundColor = 'Green'
+$Host.UI.RawUI.ForegroundColor = 'DarkGray'
+$Host.UI.RawUI.WindowTitle = “Boot Tools - v$_version”
+function prompt { 'WolfNet Computing Boot Tools> ' }
+
+Clear-Host
+Write-Host "Setting up environment for WolfNet Computing Boot Tools..."
+Write-Host "You're running version: $_version"
---
+++ boot-tools.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd "%~dp0"
+powershell.exe -NoLogo -NoExit -File boilerplate.ps1
