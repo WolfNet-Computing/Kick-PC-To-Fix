7fe6a2a90421e9506a4d365f3d1f45d42a0a39c4
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2023-08-19 09:33:48 +0100

    Update custom.ps1

--- Plugins/WolfNet-Computing-NetBoot-Disc/usbs/netboot/custom.ps1
+++ Plugins/WolfNet-Computing-NetBoot-Disc/usbs/netboot/custom.ps1
@@ -55,7 +55,7 @@ Function Download-File {
 ForEach ($directory in ".\usbs\$busbd_name\files") {
 	ForEach ($file in "$directory\kernels\ipxe.lkrn") {
 		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $url_list.Length; $i++) {
+			For ($i=0; $i -le $file_list.Length; $i++) {
 				Download-File -url "$($file_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
 				If (Test-Path -Path "$file") { Break }
 			}
@@ -72,38 +72,42 @@ New-Item -Name "_iso_" -Path $env:temp -itemType Directory
 
 # Download any ISO files necessary for the disk...
 ForEach ($directory in "$env:temp\_iso_") {
-	ForEach ($file in "$directory\dslcore-20080717.iso") {
+	ForEach ($file in "$directory\dslcore_20080717.iso") {
 		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $url_list.Length; $i++) {
+			For ($i=0; $i -le $iso_list.Length; $i++) {
 				Download-File -url "$($iso_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
 				If (Test-Path -Path "$file") { Break }
 			}
-			If (Test-Path -Path "$file") {
-				# Mount the ISO and extract necessary files from it...
-			}
-			Else {
+			If (-not (Test-Path -Path "$file")) {
 				Write-Host "CUSTOM: Couldn't download file '$file'."
 				Exit
 			}
 		}
 	}
 }
-usb140201.zip
+
+# Create the temporary directory for the downloaded ZIP files...
+New-Item -Name "_zip_" -Path $env:temp -itemType Directory
+
 # Download any ZIP files necessary for the disk...
-ForEach ($directory in "$env:temp\_iso_") {
-	ForEach ($file in "$directory\dslcore-20080717.iso") {
+ForEach ($directory in "$env:temp\_zip_") {
+	ForEach ($file in "$directory\usb140201.zip") {
 		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $url_list.Length; $i++) {
-				Download-File -url "$($iso_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
+			For ($i=0; $i -le $zip_list.Length; $i++) {
+				Download-File -url "$($zip_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
 				If (Test-Path -Path "$file") { Break }
 			}
-			If (Test-Path -Path "$file") {
-				# Extract necessary files from ZIP file...
-			}
-			Else {
+			If (-not (Test-Path -Path "$file")) {
 				Write-Host "CUSTOM: Couldn't download file '$file'."
 				Exit
 			}
 		}
 	}
 }
+
+# Mount ISOs and extract necessary files.
+
+# Extract necessary files from ZIP archives.
+
+Remove-Item -Path $env:temp\_iso_ -recurse
+Remove-Item -Path $env:temp\_zip_ -recurse
