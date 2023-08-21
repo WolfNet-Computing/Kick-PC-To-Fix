8c54cb3631328ad4946e9cb8f474def8e8523cea
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2023-08-21 13:19:19 +0100

    Update custom.ps1

--- Plugins/WolfNet-Computing-NetBoot-Disc/usbs/netboot/custom.ps1
+++ Plugins/WolfNet-Computing-NetBoot-Disc/usbs/netboot/custom.ps1
@@ -4,21 +4,21 @@
 
 .NOTES
     Author: John Wolfe
-    Last Edit: 16-08-2023 03:07
+    Last Edit: 21-08-2023 02:55
 #>
 
 #----------------[ Declarations ]----------------
 
 $file_list = @(
-	"https://wolfnet-computing.com/pxe"
+	"https://wolfnet-computing.com/pxe/ipxe.lkrn"
 )
 
 $iso_list = @(
-	"http://distro.ibiblio.org/damnsmall/dslcore"
+	"http://distro.ibiblio.org/damnsmall/dslcore/dslcore_20080717.iso"
 )
 
 $zip_list = @(
-	"http://pogostick.net/~pnh/ntpasswd"
+	"http://pogostick.net/~pnh/ntpasswd/usb140201.zip"
 )
 
 #----------------[ Functions ]------------------
@@ -49,59 +49,69 @@ Function Download-File {
 	Invoke-WebRequest -Uri $url -OutFile $filename
 }
 
+function Add-DownloadToList {
+	<#
+	.SYNOPSIS
+		This advanced function adds a location to the download list.
+
+	.PARAMETER url
+		The parameter url is used to define the URI to be downloaded from.
+
+	.PARAMETER filename
+		The parameter filename is used to define the file to download to.
+
+	.NOTES
+		Author: John Wolfe
+	#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True, Position=0)]
+        [string]$url,
+
+        [Parameter(Mandatory=$True)]
+        [string]$filename
+    )
+	If ($downloads -eq $null) {
+		$downloads = @(
+			()
+		)
+	}
+}
+
 #----------------[ Main Execution ]---------------
 
-# Download any missing files that can be downloaded...
-ForEach ($directory in ".\usbs\$busbd_name\files") {
-	ForEach ($file in "$directory\kernels\ipxe.lkrn") {
-		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $file_list.Length; $i++) {
-				Download-File -url "$($file_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
-				If (Test-Path -Path "$file") { Break }
-			}
-			If (-not (Test-Path -Path "$file")) {
-				Write-Host "CUSTOM: Couldn't download file '$file'."
-				Exit
-			}
-		}
-	}
-}
-
 # Create the temporary directory for the downloaded ISO files...
+If (Test-Path -Path "$env:temp\_iso_") { Remove-Item -Path "$env:temp\_iso_" -recurse }
 New-Item -Name "_iso_" -Path $env:temp -itemType Directory
 
-# Download any ISO files necessary for the disk...
-ForEach ($directory in "$env:temp\_iso_") {
-	ForEach ($file in "$directory\dslcore_20080717.iso") {
-		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $iso_list.Length; $i++) {
-				Download-File -url "$($iso_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
-				If (Test-Path -Path "$file") { Break }
-			}
-			If (-not (Test-Path -Path "$file")) {
-				Write-Host "CUSTOM: Couldn't download file '$file'."
-				Exit
-			}
-		}
+# Create the temporary directory for the downloaded ZIP files...
+If (Test-Path -Path "$env:temp\_zip_") { Remove-Item -Path "$env:temp\_zip_" -recurse }
+New-Item -Name "_zip_" -Path $env:temp -itemType Directory
+
+# Download any missing files that can be downloaded...
+For ($i=0; $i -lt $file_list.Length; $i++) {
+	Download-File -url "$($file_list[$i])" -filename ".\usbs\$busbd_name\files\kernels\$(([uri]$file_list[$i]).Segments[-1])"
+	If (-not (Test-Path -Path ".\usbs\$busbd_name\files\kernels\$(([uri]$file_list[$i]).Segments[-1])")) {
+		Write-Host "CUSTOM: Couldn't download file '.\usbs\$busbd_name\files\kernels\$(([uri]$file_list[$i]).Segments[-1])'."
+		Exit
 	}
 }
 
-# Create the temporary directory for the downloaded ZIP files...
-New-Item -Name "_zip_" -Path $env:temp -itemType Directory
+# Download any ISO files necessary for the disk...
+For ($i=0; $i -lt $iso_list.Length; $i++) {
+	Download-File -url "$($iso_list[$i])" -filename "$env:temp\_iso_\$(([uri]$iso_list[$i]).Segments[-1])"
+	If (-not (Test-Path -Path "$env:temp\_iso_\$(([uri]$iso_list[$i]).Segments[-1])")) {
+		Write-Host "CUSTOM: Couldn't download file '$env:temp\_iso_\$(([uri]$iso_list[$i]).Segments[-1])'."
+		Exit
+	}
+}
 
 # Download any ZIP files necessary for the disk...
-ForEach ($directory in "$env:temp\_zip_") {
-	ForEach ($file in "$directory\usb140201.zip") {
-		If (-not (Test-Path -Path "$file")) {
-			For ($i=0; $i -le $zip_list.Length; $i++) {
-				Download-File -url "$($zip_list[$i])/$([System.IO.Path]::GetFileName($file))" -filename "$file"
-				If (Test-Path -Path "$file") { Break }
-			}
-			If (-not (Test-Path -Path "$file")) {
-				Write-Host "CUSTOM: Couldn't download file '$file'."
-				Exit
-			}
-		}
+For ($i=0; $i -lt $zip_list.Length; $i++) {
+	Download-File -url "$($zip_list[$i])" -filename "$env:temp\_zip_\$(([uri]$zip_list[$i]).Segments[-1])"
+	If (-not (Test-Path -Path "$env:temp\_zip_\$(([uri]$zip_list[$i]).Segments[-1])")) {
+		Write-Host "CUSTOM: Couldn't download file '$env:temp\_zip_\$(([uri]$zip_list[$i]).Segments[-1])'."
+		Exit
 	}
 }
 
@@ -109,5 +119,5 @@ ForEach ($directory in "$env:temp\_zip_") {
 
 # Extract necessary files from ZIP archives.
 
-Remove-Item -Path $env:temp\_iso_ -recurse
-Remove-Item -Path $env:temp\_zip_ -recurse
+Remove-Item -Path "$env:temp\_iso_" -recurse
+Remove-Item -Path "$env:temp\_zip_" -recurse
